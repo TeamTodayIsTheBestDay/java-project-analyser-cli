ccd82c458cd2ff50dca6d6e25e32c1b4defb112f
1491456253 <1491456253@qq.com>
2023-10-23 21:27:48 +0800

    括号错误处理完成

--- src/main/java/team/todaybest/analyser/helper/IncompleteCodeHelper.java
+++ src/main/java/team/todaybest/analyser/helper/IncompleteCodeHelper.java
@@ -1,5 +1,7 @@
 package team.todaybest.analyser.helper;
 
+import java.util.Stack;
+
 /**
  * 不完全代码的相关工具
  *
@@ -12,6 +14,60 @@ public class IncompleteCodeHelper {
     }
 
     public static boolean reviewCode(String code, String path) {
+        Stack<Character> stack = new Stack<>();
+        boolean inSingleQuotes = false;
+        boolean inDoubleQuotes = false;
+        boolean inComment = false;
+        boolean inLineComment = false;
+
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+
+            if ((c == '\'' || c == '\"') && !inComment && !inLineComment) {//对引号要判断转义的可能性
+                int continousBackslashNum = 0;
+                while (i - continousBackslashNum - 1 >= 0 && code.charAt(i - continousBackslashNum - 1) == '\\') {
+                    continousBackslashNum++;
+                }
+                if (continousBackslashNum % 2 == 0) {//前面有连续奇数个反斜杠是转义，不用管
+                    if (c == '\'' && !inDoubleQuotes) {
+                        inSingleQuotes = !inSingleQuotes;
+                    } else if (c == '\"' && !inSingleQuotes) {
+                        inDoubleQuotes = !inDoubleQuotes;
+                    }
+                }
+            } else if (i < code.length() - 1 && c == '/' && code.charAt(i + 1) == '*' && !inSingleQuotes && !inDoubleQuotes&&!inComment&&!inLineComment) {
+                inComment = true;
+                i++;
+            } else if (i < code.length() - 1 && c == '*' && code.charAt(i + 1) == '/' && !inSingleQuotes && !inDoubleQuotes&&inComment) {
+                inComment = false;
+                i++;
+            }else if (i < code.length() - 1 && c == '/' && code.charAt(i + 1) == '/' && !inSingleQuotes && !inDoubleQuotes && !inComment&&!inLineComment) {
+                inLineComment = true;
+                i++;
+            } else if (c == '\n'&&inLineComment) {
+                inLineComment = false;
+            }
+
+
+            if (!inSingleQuotes && !inDoubleQuotes && !inComment&&!inLineComment) {
+                if (c == '(' || c == '{' || c == '[') {
+                    stack.push(c);
+                } else if (c == ')' || c == '}' || c == ']') {
+                    if (stack.isEmpty()){
+                        printReviewError("括号"+c+"不匹配",path);
+                    }
+                    char lastBracket = stack.pop();
+                    if ((c == ')' && lastBracket != '(') || (c == '}' && lastBracket != '{') || (c == ']' && lastBracket != '[')) {
+                        printReviewError("括号不对应",path);
+                    }
+                }
+            }
+
+        }
+        if(!stack.isEmpty()){
+            printReviewError("括号不匹配",path);
+        }
+
         return true;
     }
 
